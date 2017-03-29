f5482da1686581f9216907055188e1a996534624
Scott Kriss <skriss@deloitte.com>
2017-03-29 10:13:21 -0700

    starting over

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public bool scene1;
+	public bool scene2;
+	public bool scene3;
+
+	void OnTriggerEnter (Collider other) {
+		if(other.tag == "VrController")
+ 		{
+     if(scene1 == true)
+     {
+         SceneManager.LoadScene("Scene1"); //This will load the scene that is named "Floor One" change the scene name in the script
+     }
+     else if(scene2 == true)
+     {
+         SceneManager.LoadScene("Scene2"); // loads a scene called "Floor Two"
+     }
+     else if(scene3 == true)
+     {
+         SceneManager.LoadScene("Scene3");
+     }
+     else
+     {
+         Debug.Log("Multiple bools were enabled on one object");
+     }
+  }
+ }
+}
---
+++ Assets/Scripts/ControllerScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ControllerScene : MonoBehaviour {
+
+	SteamVR_TrackedObject obj;
+
+	public GameObject buttonHolder;
+
+	public bool ButtonEnabled;
+
+
+	// Use this for initialization
+	void Awake () {
+		obj = GetComponent<SteamVR_TrackedObject>();
+		buttonHolder.SetActive(false);
+		ButtonEnabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var device = SteamVR_Controller.Input((int)obj.index);
+		if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu)){
+			if(ButtonEnabled == false){
+				buttonHolder.SetActive(true);
+				ButtonEnabled = true;
+
+			}else if (ButtonEnabled == true){
+				buttonHolder.SetActive(false);
+				ButtonEnabled = false;
+			}
+		}
+	}
+}
